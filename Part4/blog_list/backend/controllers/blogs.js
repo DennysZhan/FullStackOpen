278c4407ee58a00d64e639b94e8dd80308ca0537
DennysZhan <speedythemerald2@gmail.com>
2025-06-29 21:46:50 -0700

    Completed exercises 4.8 - 4.12

--- Part4/blog_list/backend/controllers/blogs.js
+++ Part4/blog_list/backend/controllers/blogs.js
@@ -1,25 +1,30 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const body = request.body
+blogsRouter.post('/', async (request, response) => {
+  try {
+    const body = request.body
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
-  
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    })
+    
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }
+    response.status(500).json({ error: 'Something went wrong' })
+  }
 })
 
 module.exports = blogsRouter
---
+++ Part4/blog_list/backend/tests/blog_api.test.js
@@ -0,0 +1,96 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('blog ids are called id', async () => {
+  const response = await api.get('/api/blogs')
+  
+  response.body.forEach(blog => {
+    assert(blog.id !== undefined)
+    assert(blog._id === undefined)
+  })
+})
+
+test('creating a blog post', async () => {
+  const newBlog = {
+    title: "Title",
+    author: "Author",
+    url: "https://hi.com",
+    likes: 7
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const updatedList = await api.get('/api/blogs')
+  assert.strictEqual(updatedList.body.length, initialBlogs.length + 1)
+})
+
+// 4.11: Test that likes defaults to 0 when missing
+test('creating a blog post without likes defaults to 0', async () => {
+  const newBlog = {
+    title: "Blog without likes",
+    author: "Author",
+    url: "https://example.com"
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.likes, 0)
+})
+
+// 4.12: Test that missing title returns 400
+test('creating a blog post without title and url returns 400', async () => {
+  const newBlog = {
+    author: "Author",
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
